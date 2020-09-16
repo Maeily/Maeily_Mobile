25c0ef80c686b24da403436d6859d22cca4549ad
zxcmnb05 <zxcmnb05@naver.com>
2020-09-16 21:27:27 +0900

    캘린더 만들자
    1. 캘린더를 만들었다.

--- app/src/main/java/com/example/maeily_mobile/FragmentHome.java
+++ app/src/main/java/com/example/maeily_mobile/FragmentHome.java
@@ -9,9 +9,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
 public class FragmentHome extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+
     }
 }
--- app/src/main/java/com/example/maeily_mobile/FragmentSchedule.java
+++ app/src/main/java/com/example/maeily_mobile/FragmentSchedule.java
@@ -8,10 +8,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class FragmentSchedule extends Fragment {
+
+    Button button;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_schedule, container, false);
+
+
     }
 }
