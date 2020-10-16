99599d731b72d966a82b4cb94c94f7144e24cc21
zxcmnb05 <zxcmnb05@naver.com>
2020-10-16 20:54:50 +0900

    profile에 사진을 넣을수 있다

--- app/src/main/java/com/example/maeily_mobile/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/maeily_mobile/fragment/ProfileFragment.java
@@ -1,21 +1,56 @@
 package com.example.maeily_mobile.fragment;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.maeily_mobile.R;
 
+import java.io.IOException;
+
 public class ProfileFragment extends Fragment {
+    ImageView imageView;
+    Bitmap bitmap;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        imageView = view.findViewById(R.id.profile_Iv);
+        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.user_profile);
+        imageView.setImageBitmap(bitmap);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 1);
+            }
+        });
+
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri image = data.getData();
+        try {
+            bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), image);
+            imageView.setImageBitmap(bitmap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
